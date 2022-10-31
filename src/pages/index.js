1b2e78d9b118dd073bec0ded7ee612353384f22a
ClaytonDewey <clay@dryadmedia.com>
2022-10-31 14:40:44 -0500

    Setup Hero section and import to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,11 @@ import {
 } from '../components'
 
 const HomePage = () => {
-  return <h2>home page</h2>
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  )
 }
 
 export default HomePage
